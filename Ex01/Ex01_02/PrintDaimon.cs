337be19bafa70d10923437ddcfb887ef197d262d
Yael Barak <yael.b@securithings.com>
2023-03-31 15:42:52 +0300

    Yael open new file

---
+++ Ex01/Ex01_02/PrintDaimon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_02
+{
+    internal class PrintDaimon
+    {
+    }
+}
